276a28afadf20795c909e8f8ba1e7a6c403bdc54
Pedro <pepiscor@gmail.com>
2024-10-25 11:54:19 +0000

    feat: html+css+js

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from app.routes import router
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to Hoops Players Scouts. How can I help you today?"}
-
 app.include_router(router)
+# Montar archivos estáticos
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+# Cambia el método para servir el HTML en la raíz
+@app.get("/", response_class=HTMLResponse)
+async def get_index():
+    with open("app/static/index.html") as f:
+        return f.read()
--- app/routes.py
+++ app/routes.py
@@ -2,16 +2,14 @@ from fastapi import APIRouter
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from app.api import consulta_jugador
-∫
+
 #To create the routes and endpoints o hoopsplayerscoutapp
-
-
 router = APIRouter()
 
 router.add_api_route("/consulta_jugador/", consulta_jugador, methods=["POST"])
-router.mount("/static", StaticFiles(directory="app/static"), name="static")
+#router.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 @router.get("/", response_class=HTMLResponse)
 async def get_index():
     with open("app/static/index.html") as f:
-        return f.read(
+        return f.read()
