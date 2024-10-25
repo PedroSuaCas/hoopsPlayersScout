0d95bf58aec80e2cac28d08c10c4982eb300f08e
Pedro <pepiscor@gmail.com>
2024-10-25 12:04:02 +0000

    feat: inital version without button working

--- app/static/script.js
+++ app/static/script.js
@@ -13,4 +13,4 @@ async function sendMessage() {
     const data = await response.json();
     responseDiv.innerText = data.respuesta || "Error al obtener la respuesta.";
     document.getElementById("userInput").value = "";  // Limpiar la caja de texto
-}∫
+}
